9b1cd019d98225ff38c4c4b3930277f695e05f97
Santeri Sipilä <santeri.sipila@aalto.fi>
2020-11-09 12:22:37 +0200

    made the processvideo whole.

--- main.m
+++ main.m
@@ -1,4 +1,6 @@
 % main script to run the program:
 video=videoImport();
+tic
+processVideo(video);
+toc
 disp("frame rate is "+ num2str(video.FrameRate)+ " and the length is "+ num2str(video.Duration))
-%Very important change
