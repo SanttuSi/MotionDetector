8aba892502d40df7559812703b1f767845977ad2
Santeri Sipilä <santeri.sipila@aalto.fi>
2020-11-11 12:38:01 +0200

    Made structural changes

--- main.m
+++ main.m
@@ -1,6 +1,5 @@
 % main script to run the program:
+disp("This is a program that detects motion from still video such as security camera footage.")
 video=videoImport();
-tic
 processVideo(video);
-toc
 disp("frame rate is "+ num2str(video.FrameRate)+ " and the length is "+ num2str(video.Duration))
---
+++ videoExporter.m
@@ -0,0 +1,17 @@
+function [writeObj]=videoExporter()
+    count = 0;
+    err_count = 0;
+    while count == err_count
+        try
+            str = input("Please input the resulting videos file location.\n",'s');
+             writeObj=VideoWriter(str); % write the result video to str
+        catch ERR
+            disp("There was an error in trying to use the selected file location.")
+            disp(ERR)
+            disp("Please try again.")
+            err_count = err_count + 1;
+        end
+        count = count + 1;
+    end
+
+end
