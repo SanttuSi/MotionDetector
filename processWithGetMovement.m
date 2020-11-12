9046490834d18bf1103af8d7abacabe3a7ed431c
DanielNikkari <73522682+DanielNikkari@users.noreply.github.com>
2020-11-12 22:27:34 +0200

    Update processWithGetMovement.m
    processWithGetMovement palauttaa motionFrame vektorin matriisin sijaan.

--- processWithGetMovement.m
+++ processWithGetMovement.m
@@ -11,6 +11,7 @@ function [ motionFrames ] = processWithGetMovement(video)
     % variables of the video format
     maxFrames = video.NumFrames;
     frameCount=1;
+    motionMatrix=[];
     motionFrames=[];
     
     % Variables for the framematrix
@@ -39,10 +40,10 @@ function [ motionFrames ] = processWithGetMovement(video)
         % value is 1, if it's the last the value is -1 and in other cases
         % its either motionless or between start and end frame.
         if spotMotionFrames == 1
-            motionFrames(i,1) = frameCount; 
+            motionMatrix(i,1) = frameCount; 
             i = i+1;
         elseif spotMotionFrames == -1
-            motionFrames(j,2) = frameCount - 1;
+            motionMatrix(j,2) = frameCount - 1;
             j = j+1;
         end
         
@@ -50,7 +51,7 @@ function [ motionFrames ] = processWithGetMovement(video)
         % of the video to make sure that the last value of the motion frame
         % matrix is wirtten
         if j ~= i && frameCount == maxFrames-1
-            motionFrames(j,2) = maxFrames;
+            motionMatrix(j,2) = maxFrames;
             j = j+1;
         end
         % save last frame
@@ -60,18 +61,33 @@ function [ motionFrames ] = processWithGetMovement(video)
     
     % Delete all the motion frame spaces that are less than 3 frames long
     % to prevent noise induced motion stamps
-    deleteSpots = zeros(size(motionFrames,1),1);
+    deleteSpots = zeros(size(motionMatrix,1),1);
     spot = 1;
-    for n = 1:size(motionFrames,1)
-        frameContinium = motionFrames(n,2) - motionFrames(n,1);
+    for n = 1:size(motionMatrix,1)
+        frameContinium = motionMatrix(n,2) - motionMatrix(n,1);
         if frameContinium < 3
             deleteSpots(spot,1) = n;
             spot = spot + 1;
         end
     end
+    
+    
     deleteSpots(deleteSpots==0) = [];
-    motionFrames(deleteSpots(:),:) = []; 
-        
+    motionMatrix(deleteSpots(:),:) = []; 
+    fprintf('\n');
+    disp("Frames with motion:")
+    disp("(start frame, end frame)")
+    disp(motionMatrix)
+    
+    % Create a frame vector from the motion matrix.
+    spot = 1;
+    for y = 1:size(motionMatrix,1)
+        for x = motionMatrix(y,1):motionMatrix(y,2)
+            motionFrames(1,spot) = x;
+            spot = spot + 1;
+        end
+    end
+    
     disp("processing completed.")
 end
-    
+       
