a8fed60db156d5b566ab5f2283fdd2f88dba5e02
DanielNikkari <daniel.nikkari@gmail.com>
2020-11-13 13:07:12 +0200

    Refactored processWithGetMovement

--- processWithGetMovement.m
+++ processWithGetMovement.m
@@ -10,17 +10,11 @@ function [ motionFrames ] = processWithGetMovement(video)
     
     % variables of the video format
     maxFrames = video.NumFrames;
-    frameCount=1;
-    motionMatrix=[];
-    motionFrames=[];
-    
-    % Variables for the framematrix
-    i = 1;
-    j = 1;
-    spotMotionFrames = 0;
+    motionFrames=zeros(1,maxFrames);
+    motionFrameCounter=1;
     
     %loop trough each frame
-    while hasFrame(video)
+    for i=2:maxFrames
         frame = readFrame(video); %read the next frame
         grayFrame= rgb2gray(frame); % convert frame to grayscale
         diff=grayFrame-lastFrame; % calculate the difference of the frames
@@ -29,65 +23,13 @@ function [ motionFrames ] = processWithGetMovement(video)
         % Assign numeric values to check if it's the first frame or the
         % last frame of motion frames, or if it's motionless frame
         if ~isempty(diff(diff>70))
-            spotMotionFrames = spotMotionFrames+1;
-        elseif spotMotionFrames >= 1 && ~isempty(diff(diff<70))
-            spotMotionFrames = -1;
-        elseif ~isempty(diff(diff<70)) & spotMotionFrames == -1
-            spotMotionFrames = 0;
+            motionFrames(motionFrameCounter)=i;
+            motionFrameCounter=motionFrameCounter+1;
         end
         
-        % If it's the first frame of the motion frames the spotMotionFrames
-        % value is 1, if it's the last the value is -1 and in other cases
-        % its either motionless or between start and end frame.
-        if spotMotionFrames == 1
-            motionMatrix(i,1) = frameCount; 
-            i = i+1;
-        elseif spotMotionFrames == -1
-            motionMatrix(j,2) = frameCount - 1;
-            j = j+1;
-        end
-        
-        % Check that if the last motion frame is the same as the last frame
-        % of the video to make sure that the last value of the motion frame
-        % matrix is wirtten
-        if j ~= i && frameCount == maxFrames-1
-            motionMatrix(j,2) = maxFrames;
-            j = j+1;
-        end
         % save last frame
         lastFrame=grayFrame;
-        frameCount= frameCount+1;
     end
-    
-    % Delete all the motion frame spaces that are less than 3 frames long
-    % to prevent noise induced motion stamps
-    deleteSpots = zeros(size(motionMatrix,1),1);
-    spot = 1;
-    for n = 1:size(motionMatrix,1)
-        frameContinium = motionMatrix(n,2) - motionMatrix(n,1);
-        if frameContinium < 3
-            deleteSpots(spot,1) = n;
-            spot = spot + 1;
-        end
-    end
-    
-    
-    deleteSpots(deleteSpots==0) = [];
-    motionMatrix(deleteSpots(:),:) = []; 
-    fprintf('\n');
-    disp("Frames with motion:")
-    disp("(start frame, end frame)")
-    disp(motionMatrix)
-    
-    % Create a frame vector from the motion matrix.
-    spot = 1;
-    for y = 1:size(motionMatrix,1)
-        for x = motionMatrix(y,1):motionMatrix(y,2)
-            motionFrames(1,spot) = x;
-            spot = spot + 1;
-        end
-    end
-    
+    motionFrames = motionFrames(1:motionFrameCounter);
     disp("processing completed.")
 end
-       
