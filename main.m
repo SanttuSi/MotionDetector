5bce345140aec3a5e787b12dccc82267de976280
DanielNikkari <daniel.nikkari@gmail.com>
2020-11-29 15:01:35 +0200

    Siiretty timestamp printtauksen mainiin

--- main.m
+++ main.m
@@ -2,5 +2,12 @@
 disp("This is a program that detects motion from still video such as security camera footage.")
 video=videoImport();
 motionFrames=processVideo(video);
-framesToTimestamps(motionFrames, video); % printtaus
+timeStamps=framesToTimestamps(motionFrames, video); % printtaus
+% Printing the resulting timestamps
+fprintf('\n');
+disp("Time spaces with motion")
+disp("In seconds:")
+disp("------------------------")
+disp("(start time, end time)")
+disp(timeStamps)
 disp("frame rate is "+ num2str(video.FrameRate)+ " and the length is "+ num2str(video.Duration))
